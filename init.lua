877e50ad298812ea1ba060fc455f585bbcf7a3be
Ssnibles <joshua.breite@gmail.com>
2024-09-26 13:15:08 +1200

    Update

--- init.lua
+++ init.lua
@@ -1,15 +1,18 @@
-vim.g.have_nerd_font = true
+--    __    _     __        __           __             _
+--   / /__ (_)___/ /__ ___ / /____ _____/ /_  ___ _  __(_)_ _
+--  /  '_// / __/  '_/(_-</ __/ _ `/ __/ __/ / _ \ |/ / /  ' \
+-- /_/\_\/_/\__/_/\_\/___/\__/\_,_/_/  \__(_)_//_/___/_/_/_/_/
 
 --  To check the current status of your plugins, run
 --    :Lazy
 --  To update plugins you can run
 --    :Lazy update
 
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+-- Bootstrap lazy plugin manager
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  local out = vim.fn.system { "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath }
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
@@ -22,42 +25,46 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Setup lazy.nvim
-require("lazy").setup({
+-- Setup lazy
+require("lazy").setup {
   spec = {
-    -- import your plugins
+    -- Import plugins from folder
     { import = "plugins" },
   },
   -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
+  -- Colorscheme that will be used when installing plugins.
   install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates
+  -- Automatically check for plugin updates
   checker = { enabled = true },
-})
+}
 
 ui = {
   -- If you are using a Nerd Font: set icons to an empty table which will use the
   -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
   icons = vim.g.have_nerd_font and {} or {
-  cmd = "⌘",
-  config = "🛠",
-  event = "📅",
-  ft = "📂",
-  init = "⚙",
-  keys = "🗝",
-  plugin = "🔌",
-  runtime = "💻",
-  require = "🌙",
-  source = "📄",
-  start = "🚀",
-  task = "📌",
-  lazy = "💤 ",
- },
+    cmd = "⌘",
+    config = "🛠",
+    event = "📅",
+    ft = "📂",
+    init = "⚙",
+    keys = "🗝",
+    plugin = "🔌",
+    runtime = "💻",
+    require = "🌙",
+    source = "📄",
+    start = "🚀",
+    task = "📌",
+    lazy = "💤 ",
+  },
 }
+
+--                                 ____ __
+--   ___ ___  __ _____________    / _(_) /__ ___
+--  (_-</ _ \/ // / __/ __/ -_)  / _/ / / -_|_-<
+-- /___/\___/\_,_/_/  \__/\__/  /_//_/_/\__/___/
 
 vim.loader.enable()
 
-require ("binds")
-require ("settings")
-require ("autocommands")
-
+require "binds"
+require "settings"
+require "autocommands"
