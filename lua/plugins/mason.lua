8ad0574530ecb0bb4a87e65e6fadd9e20ef9bd62
Ssnibles <joshua.breite@gmail.com>
2024-10-13 00:33:12 +1300

    update - new README

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,44 +1,70 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-  },
-  cmd = "Mason",
-  event = "BufReadPre",
-  opts = {
-    ui = {
-      icons = {
-        package_installed = "✓",
-        package_pending = "➜",
-        package_uninstalled = "✗",
-      },
-    },
-  },
-  config = function(_, opts)
-    require("mason").setup(opts)
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		"neovim/nvim-lspconfig",
+		"hrsh7th/cmp-nvim-lsp",
+	},
+	event = { "BufReadPre", "BufNewFile" },
+	cmd = { "Mason", "MasonInstall", "MasonUninstall", "MasonUninstallAll", "MasonLog" },
+	opts = {
+		sort = {
+			frecency = true,
+			user_items = true,
+		},
+		ui = {
+			icons = {
+				package_installed = "✓",
+				package_pending = "➜",
+				package_uninstalled = "✗",
+			},
+		},
+	},
+	config = function(_, opts)
+		require("mason").setup(opts)
 
-    require("mason-lspconfig").setup {
-      automatic_installation = true,
-      ensure_installed = {
-        "cssls",
-        "eslint",
-        "html",
-        "jsonls",
-        "pyright",
-        "tailwindcss",
-      },
-    }
+		require("mason-lspconfig").setup({
+			automatic_installation = true,
+			ensure_installed = {
+				"cssls",
+				"eslint",
+				"html",
+				"jsonls",
+				"tsserver",
+				"pyright",
+				"tailwindcss",
+			},
+		})
 
-    require("mason-tool-installer").setup {
-      ensure_installed = {
-        "prettier",
-        "stylua",
-        "isort",
-        "black",
-        "pylint",
-        "eslint_d",
-      },
-    }
-  end,
+		require("mason-tool-installer").setup({
+			ensure_installed = {
+				"prettier",
+				"stylua",
+				"isort",
+				"black",
+				"pylint",
+				"eslint_d",
+			},
+		})
+
+		local lspconfig = require("lspconfig")
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+		local servers = {
+			"cssls",
+			"eslint",
+			"html",
+			"jsonls",
+			"tsserver",
+			"pyright",
+			"tailwindcss",
+			-- Remove "ast_grep", "black", "html-lsp", "css-lsp" as they are not standard LSP servers
+		}
+		for _, lsp in ipairs(servers) do
+			lspconfig[lsp].setup({
+				capabilities = capabilities,
+			})
+		end
+	end,
 }
