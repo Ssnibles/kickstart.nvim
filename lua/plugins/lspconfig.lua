d41b3895867309f692c2745daae7a140c7d37890
Ssnibles <joshua.breite@gmail.com>
2025-01-28 02:32:29 +1300

    update

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -13,7 +13,9 @@ return {
     require("neodev").setup()
 
     local on_attach = function(client, bufnr)
-      local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+      local function buf_set_keymap(...)
+        vim.api.nvim_buf_set_keymap(bufnr, ...)
+      end
       local opts = { noremap = true, silent = true }
 
       buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
@@ -60,7 +62,7 @@ return {
       "eslint",
       "html",
       "jsonls",
-      "tsserver",
+      "ts_ls",
       "pyright",
       "tailwindcss",
       "bashls",
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -27,7 +27,7 @@ return {
         "eslint",
         "html",
         "jsonls",
-        "tsserver",
+        "ts_ls",
         "pyright",
         "tailwindcss",
         "bashls",
