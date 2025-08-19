526b0e21121e0bd2cc6abdb2635329c377b51686
Ssnibles <joshua.breite@gmail.com>
2025-08-19 23:14:12 +1200

    ✨ Better oil config

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,14 +1,44 @@
 return {
   "stevearc/oil.nvim",
-  opts = {
-    default_file_explorer = true, -- Replace netrw
-    view_options = {
-      show_hidden = true, -- Show dotfiles
-    },
-    keymaps = {
-      ["q"] = "actions.close", -- Close with q
-    },
-  },
-  dependencies = { "nvim-tree/nvim-web-devicons" }, -- Optional, for file icons
-}
+  config = function()
+    local oil = require("oil")
+    oil.setup({
+      -- Recommended: Replace netrw with Oil
+      default_file_explorer = true,
+      -- Configure view options
+      view_options = {
+        show_hidden = true,
+      },
+      -- Configure keymaps
+      keymaps = {
+        -- Navigate
+        ["<C-h>"] = "actions.parent_directory",
+        ["-"] = "actions.parent_directory",
+        ["~"] = "actions.cd_home",
+        ["<C-l>"] = "actions.refresh",
+        -- Actions
+        ["g?"] = "actions.show_help",
+        ["<Cr>"] = "actions.select",
+        ["<C-s>"] = "actions.select_vsplit",
+        ["<C-t>"] = "actions.select_tab",
+        ["<C-p>"] = "actions.preview",
+        ["<C-c>"] = "actions.close",
+        ["<C-e>"] = "actions.close",
+        ["<C-d>"] = "actions.remove",
+        ["<C-r>"] = "actions.rename",
+        ["<C-n>"] = "actions.new_file",
+        ["<C-N>"] = "actions.new_dir",
+      },
+    })
 
+    vim.keymap.set("n", "<leader>o", function()
+      if vim.bo.filetype == "oil" then
+        oil.close()
+      else
+        oil.open()
+      end
+    end, { desc = "Toggle Oil" })
+
+  end,
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+}
